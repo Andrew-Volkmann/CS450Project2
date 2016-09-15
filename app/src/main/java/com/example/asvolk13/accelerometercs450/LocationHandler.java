151c063b11877f664de1dfe32d148019bab422a8
Andrew Volkmann <asvolk13@gmail.com>
2016-09-15 09:30:31 -0400

    final version of app

--- app/src/main/java/com/example/asvolk13/accelerometercs450/LocationHandler.java
+++ app/src/main/java/com/example/asvolk13/accelerometercs450/LocationHandler.java
@@ -3,12 +3,12 @@ package com.example.asvolk13.accelerometercs450;
 import java.util.Observable;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 
@@ -16,14 +16,54 @@ import android.support.v4.app.ActivityCompat;
  * Created by asvolk13 on 9/13/2016.
  */
 public class LocationHandler extends Observable {
-    private Location location;
+
     private LocationManager locationManager;
     private LocationListener locationListener;
-    MainActivity act;
+    private Location location;
 
-    public LocationHandler(MainActivity act) {
-        this.act = act;
-        locationManager = (LocationManager) act.getSystemService(Context.LOCATION_SERVICE);
+    Activity act;
+    private static final long DISTANCE_TO_UPDATE = 0;
+    private static final long TIME_BETWEEN_UPDATES = 0;
+
+    public LocationHandler(Activity act) {
+        this.act = (MainActivity) act;
+        locationManager = (LocationManager) this.act.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+
+            @Override
+            public void onLocationChanged(Location location) {
+                setChanged();
+                notifyObservers(location);
+            }
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+            @Override
+            public void onProviderDisabled(String provider) {
+            }
+        };
+        while (ActivityCompat.checkSelfPermission(this.act, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.act, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.checkSelfPermission(this.act, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.act, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                this.act.requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+                this.act.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+            }
+
+        }
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, TIME_BETWEEN_UPDATES, DISTANCE_TO_UPDATE, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, TIME_BETWEEN_UPDATES, DISTANCE_TO_UPDATE, locationListener);
+
+        this.location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        setChanged();
+        notifyObservers(location);
 
     }
+    public Location getLocation(){
+        return location;
+    }
 }
+
--- app/src/main/java/com/example/asvolk13/accelerometercs450/MainActivity.java
+++ app/src/main/java/com/example/asvolk13/accelerometercs450/MainActivity.java
@@ -1,26 +1,48 @@
 package com.example.asvolk13.accelerometercs450;
 
 
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.IllegalFormatConversionException;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.zip.DataFormatException;
 
 public class MainActivity extends AppCompatActivity implements Observer {
+    public static final String LONGITUDE = "LONGITUDE";
+    public static final String LATITUDE = "LATITUDE";
+    public static final String X = "X";
+    public static final String Y = "Y";
+    public static final String Z = "Z";
+
     TextView z_accel_view = null;
     TextView x_accel_view = null;
     TextView y_accel_view = null;
+    TextView longitude_view = null;
+    TextView latitude_view = null;
     AccelerometerHandler ah = null;
+    LocationHandler lh = null;
+    Location location;
+    String lat;
+    String lon;
+    float[] xyz;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         this.z_accel_view = (TextView) findViewById(R.id.z_accel_view);
         this.x_accel_view = (TextView) findViewById(R.id.x_accel_view);
         this.y_accel_view = (TextView) findViewById(R.id.y_accel_view);
+        this.longitude_view = (TextView) findViewById(R.id.Longitude_view);
+        this.latitude_view = (TextView) findViewById(R.id.Latitude_view);
 
 
     }
@@ -30,15 +52,56 @@ public class MainActivity extends AppCompatActivity implements Observer {
         super.onStart();
         this.ah = new AccelerometerHandler(this);
         this.ah.addObserver(this);
+        this.lh = new LocationHandler(this);
+        this.lh.addObserver(this);
+
+        this.lat = getPreferences(MODE_PRIVATE).getString(LATITUDE, "");
+        this.lon = getPreferences(MODE_PRIVATE).getString(LONGITUDE, "");
+        this.latitude_view.setText(lat);
+        this.longitude_view.setText(lon);
+
+        this.x_accel_view.setText(getPreferences(MODE_PRIVATE).getString(X, ""));
+        this.y_accel_view.setText(getPreferences(MODE_PRIVATE).getString(Y, ""));
+
+        this.z_accel_view.setText(getPreferences(MODE_PRIVATE).getString(Z, ""));
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //save
+        getPreferences(MODE_PRIVATE).edit().putString(LATITUDE, Double.toString(lh.getLocation().getLatitude())).apply();
+
+        getPreferences(MODE_PRIVATE).edit().putString(LONGITUDE, Double.toString(lh.getLocation().getLongitude())).apply();
+
+        //save xyz
+        getPreferences(MODE_PRIVATE).edit().putString(X, Float.toString(xyz[0])).apply();
+        getPreferences(MODE_PRIVATE).edit().putString(Y, Float.toString(xyz[1])).apply();
+        getPreferences(MODE_PRIVATE).edit().putString(Z, Float.toString(xyz[2])).apply();
+
+
+
+
     }
 
     @Override
     public void update(Observable observable, Object o) {
 
-        float [] xyz = (float []) o;
-        this.z_accel_view.setText(Float.toString(xyz[2]));
-        this.x_accel_view.setText(Float.toString(xyz[0]));
-        this.y_accel_view.setText(Float.toString(xyz[1]));
+        if (observable instanceof AccelerometerHandler) {
+            this.xyz = (float[]) o;
+
+            this.z_accel_view.setText(Float.toString(xyz[2]));
+            this.x_accel_view.setText(Float.toString(xyz[0]));
+            this.y_accel_view.setText(Float.toString(xyz[1]));
+        }
+
+        if (observable instanceof LocationHandler) {
+            this.location = (Location) o;
+            this.lat= Double.toString(location.getLatitude());
+            this.lon= Double.toString(location.getLongitude());
+            this.latitude_view.setText(lat);
+            this.longitude_view.setText(lon);
+    }
+
     }
 }
 
